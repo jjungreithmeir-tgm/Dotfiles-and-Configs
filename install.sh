09c34331565889ba98350e4b5cc0a87800466b1f
Jakob <jjungreithmeir@student.tgm.ac.at>
2016-11-26 20:58:52 +0000

    Renamed xmodmap and aliases, added install file and modified maxima script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Calls all scripts and sets up necessary symlinks to this repository
+REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sf $REPO/.Xmodmap ~/.Xmodmap
+./maxima_tempfiles.sh
+ln -sf $REPO/.aliases ~/.aliases
+# Only adds line if it is not to be found in the zshrc
+grep -q -F "source \$(readlink ~/.aliases)" ~/.zshrc || echo "source \$(readlink ~/.aliases)" >> ~/.zshrc
--- maxima_tempfiles.sh
+++ maxima_tempfiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Preventing maxima from spamming your home directory with gnuplot and maxout files
-echo "maxima_tempdir:\"/tmp\";" >> ~/.maxima/maxima-init.mac
+touch ~/.maxima/maxima-init.mac
+grep -q -F "maxima_tempdir:\"/tmp\";" ~/.maxima/maxima-init.mac || echo "maxima_tempdir:\"/tmp\";" >> ~/.maxima/maxima-init.mac
