6fb14223ecdfcab494cb55af38bfc4d4d4961034
Jakob <jjungreithmeir@student.tgm.ac.at>
2017-01-10 09:27:19 +0000

    Move resources into proper folder.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sf $REPO/.Xmodmap ~/.Xmodmap
 ./maxima_tempfiles.sh
-ln -sf $REPO/.aliases ~/.aliases
+ln -sf $REPO/resources/.aliases ~/.aliases
 # Only adds line if it is not to be found in the zshrc
 grep -q -F "source \$(readlink ~/.aliases)" ~/.zshrc || echo "source \$(readlink ~/.aliases)" >> ~/.zshrc
 # Disabled as infinality is no longer maintained ./infinality.sh
