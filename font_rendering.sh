4d642609fa766606a5a5e6ad7ca34673b52adea0
Jakob <jjungreithmeir@student.tgm.ac.at>
2017-01-22 09:00:34 +0000

    Update font rendering script.

--- font_rendering.sh
+++ font_rendering.sh
@@ -3,5 +3,6 @@ if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
+pacman -S --asdeps --noconfirm freetype2 cairo fontconfig
 cp resources/local.conf /etc/fonts/.
 grep -q -F "export FREETYPE_PROPERTIES=\"truetype:interpreter-version=38\"" /etc/profile.d/freetype.sh || echo "export FREETYPE_PROPERTIES=\"truetype:interpreter-version=38\"" >> /etc/profile.d/freetype.sh
