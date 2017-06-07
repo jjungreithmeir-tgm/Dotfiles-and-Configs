24e2fbcd582e7c865a09f5ce865275c68eae9454
Jakob <jjungreithmeir@student.tgm.ac.at>
2017-06-08 01:35:40 +0200

    Add Teamspeak backup script

--- teamspeak3/ts3backup.sh
+++ teamspeak3/ts3backup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
-
-installation_folder = /home/teamspeak/teamspeak3-server_linux-amd64/
+installation_folder=/home/teamspeak/teamspeak3-server_linux-amd64/
+synchronized_folder=/home/teamspeak/Backup/
+fingerprint=<insert public key>
+user_id=<insert user id>
 
 tar -zcvf ${installation_folder}ts3backup.tar \
 ${installation_folder}query_ip_blacklist.txt \
 ${installation_folder}query_ip_whitelist.txt \
-${installation_folder}ts3server.sqlitedb ts3server.ini \
+${installation_folder}ts3server.sqlitedb \
+${installation_folder}ts3server.ini \
 ${installation_folder}files/internal \
 /etc/systemd/system/teamspeak.service
+
+gpg --keyserver pgp.mit.edu --recv-keys ${fingerprint}
+gpg --batch --yes --trust-model always --output ${synchronized_folder}backup.gpg --encrypt --recipient ${user_id} ${installation_folder}ts3backup.tar
+
+cd ${synchronized_folder}
+git add -A
+git commit -m "Update backup"
+HOME=/home/teamspeak git push
